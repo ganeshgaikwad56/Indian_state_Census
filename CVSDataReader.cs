1706702b77d22aa5baba59a0e5929b257dd88ac9
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-22 07:06:15 +0530

    Ganesh adding census indian state program files

---
+++ CVSDataReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensus
+{
+    public class CVSDataReader
+    {
+        public Censuse readfromcsv(string path, string country)
+        {
+            if (country == "india")
+            {
+
+            }
+        }
+    }
+}
